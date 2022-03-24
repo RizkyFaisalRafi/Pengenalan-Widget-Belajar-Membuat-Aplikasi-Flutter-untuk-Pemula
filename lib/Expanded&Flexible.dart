c43e4750720abb09c62310d7a0134fa2403a6385
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2022-03-24 08:59:21 +0700

    update

--- lib/Expanded&Flexible.dart
+++ lib/Expanded&Flexible.dart
@@ -10,11 +10,15 @@ class myApp1 extends StatelessWidget{
     return MaterialApp(
       title: 'ListView',
       theme: ThemeData(),
-      home: Rainbow(),
+      home: ExpandedFlexiblePage(),
     );
   }
 }
 
+
+// EXPANDED
+// Flutter memiliki widget Expanded yang dapat mengembangkan child dari Row atau Column sesuai dengan ruang yang tersedia.
+// Expanded memiliki parameter flex yang memiliki nilai default 1.
 class Rainbow extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -42,6 +46,7 @@ class Rainbow extends StatelessWidget{
             ),
           ),
           Expanded(
+            flex: 2,
             child: Container(
               color: Colors.blue,
             ),
@@ -61,5 +66,95 @@ class Rainbow extends StatelessWidget{
       ),
     );
   }
+}
 
+
+// Flexible
+// widget Flexible digunakan untuk mengatur ukuran widget di dalam Row atau Column secara fleksibel.
+// Perbedaan Flexible dan Expanded adalah widget Flexible memungkinkan child widget-nya berukuran lebih kecil dibandingkan ukuran ruang yang tersisa.
+// Sementara, child widget dari Expanded harus menempati ruang yang tersisa dari Column atau Row.
+class ExpandedFlexiblePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ExpandedWidget(),
+                FlexibleWidget(),
+              ],
+            ),
+            Row(
+              children: [
+                ExpandedWidget(),
+                ExpandedWidget(),
+              ],
+            ),
+            Row(
+              children: [
+                FlexibleWidget(),
+                FlexibleWidget(),
+              ],
+            ),
+            Row(
+              children: [
+                FlexibleWidget(),
+                ExpandedWidget(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExpandedWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.teal,
+          border: Border.all(color: Colors.white),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Expanded',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FlexibleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.tealAccent,
+          border: Border.all(color: Colors.white),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Flexible',
+            style: TextStyle(
+              color: Colors.teal,
+              fontSize: 24,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
